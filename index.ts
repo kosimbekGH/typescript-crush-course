75a3e9eb31304f98bb1e718bad4f3855665a100a
kosimbekGH <kosimbekkosimov1999@gmail.com>
2022-05-20 15:36:06 +0500

    1 file modified

--- index.ts
+++ index.ts
@@ -5,3 +5,51 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
+// Turple
+let turple: [number] = [2];
+
+// Union
+let unionType: number | string;
+
+// Interface
+interface PersonInterface {
+  id: number;
+  name: string;
+  register(): string;
+}
+
+// Class
+class Person implements PersonInterface {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  register() {
+    return `${this.name} is registered!`;
+  }
+}
+
+const brad = new Person(1, 'Brad');
+
+
+
+
+
+
+
+
+
+
+
+// Generics
+
+function getArray(items: any[]): any[] {
+  return new Array().concat(items);
+}
+
+let numArray = getArray([1,2,3,4,5]);
+let strArray = getArray(['Brad', 'John', 'Jack']);
